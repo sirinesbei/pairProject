b66a4dc6dff1425f22f9a46cb0081b23ee3e1060
sirinesbei <sirine.sbei119@gmail119.com>
2025-01-08 22:06:47 +0100

    done

--- js/dog.js
+++ js/dog.js
@@ -1,20 +1,14 @@
 $(document).ready(function () {
     $('#add').on('click', function () {
+       
+       var name = $('#name').val().trim();
+       var age = $('#age').val();
+       var breed = $('#breed').val().trim();
+       var weight = $('#weight').val();
+       var vaccine = $('#vaccine').val().trim();
+       var imageFile = $('#image')[0].files[0];
 
-      
-        var name = $('#name').val().trim();
-        var age = $('#age').val().trim();
-        var breed = $('#breed').val().trim();
-        var weight = $('#weight').val().trim();
-        var vaccine = $('#vaccine').val().trim();
-        var imageFile = $('#image')[0].files[0];
-
-
-
-console.log(name);
-
-        alert('Dog added successfully!');
-
-
-    })
-})
+       
+       alert('Dog details added successfully!');
+    )}
+)}

f3ca830287ac4dc76069ed40a0ef55338aba42df
mohamed-ali <mohamedalitimoumi4@gmail.com>
2025-01-08 16:38:14 +0100

    sirine

--- js/index.js
+++ js/index.js
@@ -14,3 +14,6 @@ var dog3=("doudou",2,3,"true","Havanese")
 var dog4=("blue",2,25,"true","husky")
 var dog5=("miky",2,10,"false","rod")
 var dog6=("diablo",3,20,"true","doberman")
+
+
+var dogs=[dog1,dog2,dog3,dog5,dog6]

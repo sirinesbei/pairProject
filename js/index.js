5411d95dc499c6220f95e9d61a5650acb2576cef
sirinesbei <sirine.sbei119@gmail119.com>
2025-01-09 09:23:47 +0100

    css js done 50%

--- js/index.js
+++ js/index.js
@@ -1,80 +1,80 @@
-<<<<<<< HEAD
-=======
+// <<<<<<< HEAD
+// =======
 
 
-var touhemi = JSON.parse(localStorage.getItem('data')) || []
+// var touhemi = JSON.parse(localStorage.getItem('data')) || []
 
-var dogs =[
-    {
-        name:"rex",
-        age:3,
-        weight:20,
-        vaccine:"true",
-        breed:"German shepherd",
-        image:"../images/berger almand.jpg"
+// var dogs =[
+//     {
+//         name:"rex",
+//         age:3,
+//         weight:20,
+//         vaccine:"true",
+//         breed:"German shepherd",
+//         image:"../images/berger almand.jpg"
         
-    },
-    {
-        name:"charles",
-        age:3,
-        weight:20,
-        vaccine:"true",
-        breed:"Golden retriver",
-        image:"../images/golden retriver.jbluepg"
-    },
-    {
-        name:"doudou",
-        age:1,
-        weight:20,
-        vaccine:"true",
-        breed:"Havanese",
-        image:"../images/bichon.jpg"
-    },
-    {
+//     },
+//     {
+//         name:"charles",
+//         age:3,
+//         weight:20,
+//         vaccine:"true",
+//         breed:"Golden retriver",
+//         image:"../images/golden retriver.jbluepg"
+//     },
+//     {
+//         name:"doudou",
+//         age:1,
+//         weight:20,
+//         vaccine:"true",
+//         breed:"Havanese",
+//         image:"../images/bichon.jpg"
+//     },
+//     {
 
-        name:"blue",
-        age:2,
-        weight:25,
-        vaccine:"true",
-        breed:"husky",
-        image:"../images/berger almand.jpg"
-    },
-    {
-        name:"miky",
-        age:2,
-        weight:10,
-        vaccine:"false",
-        breed:"rod",
-        image:"../images/rod.jpg"
-    },
-    {
-        name:"diablo",
-        age:3,
-        weight:20,
-        vaccine:"true",
-        breed:"doberman",
-        image:"../images/doberman.webp"
-    }
-]
+//         name:"blue",
+//         age:2,
+//         weight:25,
+//         vaccine:"true",
+//         breed:"husky",
+//         image:"../images/berger almand.jpg"
+//     },
+//     {
+//         name:"miky",
+//         age:2,
+//         weight:10,
+//         vaccine:"false",
+//         breed:"rod",
+//         image:"../images/rod.jpg"
+//     },
+//     {
+//         name:"diablo",
+//         age:3,
+//         weight:20,
+//         vaccine:"true",
+//         breed:"doberman",
+//         image:"../images/doberman.webp"
+//     }
+// ]
 
-var button = document.getElementById("search");
-        var dogs = document.getElementsByClassName("dog-card");
+// var button = document.getElementById("search");
+//         var dogs = document.getElementsByClassName("dog-card");
 
-        button.onclick = function () {
-            var input = document.getElementById("breed").value.toLowerCase();
+//         button.onclick = function () {
+//             var input = document.getElementById("breed").value.toLowerCase();
 
-            for (var i = 0; i < dogs.length; i++) {
-                var breed = dogs[i].getElementsByTagName("p")[0].textContent.toLowerCase();
+//             for (var i = 0; i < dogs.length; i++) {
+//                 var breed = dogs[i].getElementsByTagName("p")[0].textContent.toLowerCase();
 
-                if (breed.includes(input)) {
-                    dogs[i].style.display = "block";
-                } else {
-                    dogs[i].style.display = "none";
-                }
-            }
-        };
+//                 if (breed.includes(input)) {
+//                     dogs[i].style.display = "block";
+//                 } else {
+//                     dogs[i].style.display = "none";
+//                 }
+//             }
+//         };
 
-        //search worked ///
+//         //search worked ///
 
 
   
@@ -86,4 +86,4 @@ var button = document.getElementById("search");
 
 
 
->>>>>>> 6e629cdfedeab801e532b4329d1641c3ba1f8636
+// >>>>>>> 6e629cdfedeab801e532b4329d1641c3ba1f8636

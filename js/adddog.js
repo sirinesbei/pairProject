a0b2e3b64dfc35d757c552b244e6529dddfecd42
sirinesbei <sirine.sbei119@gmail119.com>
2025-01-09 10:35:27 +0100

    70% of work

--- js/adddog.js
+++ js/adddog.js
@@ -1,21 +1,21 @@
 
 var dogs = JSON.parse(localStorage.getItem('dogs')) || [
-    { name: "rex", age: 3, weight: 20, vaccine: "true",
+    { name: "Rex", age: 3, weight: 20, vaccine: "true",
          breed: "German shepherd", image: "../images/berger almand.jpg" },
          
-    { name: "charles", age: 1, weight: 20, vaccine: "true", breed: "Golden retriever",
+    { name: "Charles", age: 1, weight: 20, vaccine: "true", breed: "Golden retriever",
          image: "../images/golden retriver.jpg" },
 
 
-    { name: "doudou", age: 1, weight: 20, vaccine: "true", breed: "Havanese",
+    { name: "Doudou", age: 1, weight: 20, vaccine: "true", breed: "Havanese",
          image: "../images/bichon.jpg" },
 
-    { name: "blue", age: 2, weight: 25, vaccine: "true", breed: "Husky", image: "../images/husky.jpg" },
+    { name: "Blue", age: 2, weight: 25, vaccine: "true", breed: "Husky", image: "../images/husky.jpg" },
 
-    { name: "miky", age: 2, weight: 10, vaccine: "false", breed: "Rod",
+    { name: "Miky", age: 2, weight: 10, vaccine: "false", breed: "Rod",
          image: "../images/rod.jpg" },
 
-    { name: "diablo", age: 3, weight: 20, vaccine: "true",
+    { name: "Diablo", age: 3, weight: 20, vaccine: "true",
          breed: "Doberman", image: "../images/doberman.webp" },
 ]
 
@@ -51,15 +51,14 @@ $(document).ready(function () {
     dogs.forEach((dog) => {
         $(".listDogs").append(`
             <div class="dog-card">
-                <img src="${dog.image}" alt="${dog.breed}" class="dog-image">
-                <h3>${dog.name}</h3>
-                <p>${dog.breed}</p>
-                <p>Age: ${dog.age}</p>
-                <p>Weight: ${dog.weight}kg</p>
-                <p>Vaccination: ${dog.vaccine}</p>
+                <img src="${dog.image}" alt="cute dog for adoption" class="dog-image"><br>
+                <h3>${dog.name}</h3><br>
+                <strong>${dog.breed}</strong><br>
+                <strong>Age: ${dog.age}</strong><br>
+                <strong>Weight: ${dog.weight}kg</strong><br>
+                <strong id="vacc">Vaccination: ${dog.vaccine}</strong>
                 
-            </div>
-        `)
+            </div>`)
     })
 })
 
